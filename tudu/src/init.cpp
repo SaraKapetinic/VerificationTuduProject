32a55f7fa99988e9ab4c294d4414ac909b650c18
Dimitrije Stamenic <stamenicdimitrije@gmail.com>
2020-12-16 18:12:00 +0100

    Setup the selection of current time of the day
    - On startup of the applicaion, current time of the day will
    be slected by default
    - Changed the way that vertical headers will be shown
    - Added priority field in the Add Task Form

--- tudu/src/init.cpp
+++ tudu/src/init.cpp
@@ -7,7 +7,7 @@ void Init::setDays(Ui::MainWindow* ui){
     QDate currentDate = ui->calendarMonths->selectedDate();
 
     // Ordinal number of current day (Monday = 1; ... ; Sunday = 7)
-    int currentDayOfWeek = currentDate.dayOfWeek();
+    int currentDayOfWeek = getCurrentDayOfWeek(ui);
 
     // Highlight current day header
     QColor headerColor = (255);
@@ -33,10 +33,14 @@ void Init::setDays(Ui::MainWindow* ui){
 
 }
 
-void Init::setHeaders(Ui::MainWindow *ui){
+void Init::setHeaders(Ui::MainWindow* ui){
+
+    // Ordinal number of current day (Monday = 1; ... ; Sunday = 7)
+    int currentDayOfWeek = getCurrentDayOfWeek(ui);
 
     // Get current hour
     int currentHour = QTime::currentTime().hour();
+    int currentMinutes = QTime::currentTime().minute();
 
     // Set column headers
     ui->tableWidget->setHorizontalHeaderLabels(m_horizontalHeaders);
@@ -46,20 +50,50 @@ void Init::setHeaders(Ui::MainWindow *ui){
     for (int i=0; i<HOURS_IN_DAY; i++) {
         for (int j=0; j<MINUTES_IN_HOUR; j+=MINUTE_INCREMENTS) {
             time = "";
-            time.sprintf("%02d.%02d", i, j);
+            if(j == 0) {
+                time.sprintf("%02d:%02d", i, j);
+            }
             m_verticalHeaders << time;
         }
     }
+    std::cout << currentMinutes%15 << std::endl;
     ui->tableWidget->setVerticalHeaderLabels(m_verticalHeaders);
-    ui->tableWidget->verticalHeaderItem(4*currentHour)->setBackground(QBrush(QColor("red")));
-
+    ui->tableWidget->verticalHeaderItem(getCurrentTimeRow(ui))->setBackground(QBrush(QColor("red")));
+    ui->tableWidget->setCurrentCell(getCurrentTimeRow(ui), currentDayOfWeek-1);
 
 }
 
-void Init::setDesign(Ui::MainWindow *ui){
+void Init::setDesign(Ui::MainWindow *ui) {
     ui->tableWidget->setStyleSheet("background-color: red;");
 }
 
-QList<QDate> Init::getCurrentWeek(){
+QList<QDate> Init::getCurrentWeek() {
     return m_currentWeek;
+}
+
+int Init::getCurrentTimeRow(Ui::MainWindow* ui) {
+
+    // Get current hour
+    int currentHour = QTime::currentTime().hour();
+    // Get cuurent minutes -> in range [0,59]
+    int currentMinutes = QTime::currentTime().minute();
+
+    int minuteQuadrants;
+    if (currentMinutes < 60) {
+        minuteQuadrants = 3;
+    } else if (currentMinutes <= 45) {
+        minuteQuadrants = 2;
+    } else if (currentMinutes <= 30) {
+        minuteQuadrants = 1;
+    } else {
+        minuteQuadrants = 0;
+    }
+
+    return 4*currentHour + minuteQuadrants;
+}
+
+int Init::getCurrentDayOfWeek(Ui::MainWindow* ui) {
+    QDate currentDate = ui->calendarMonths->selectedDate();
+
+    return currentDate.dayOfWeek();
 }
